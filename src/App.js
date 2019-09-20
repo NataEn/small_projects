8267c591d5b8c8466c9c31754f481c6c5a4ebb18
Natalie <natalie@enkin.co.il>
2019-09-20 22:45:46 +0300

    setting al components, buttons not rerendering the chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,183 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import cellEditFactory from "react-bootstrap-table2-editor";
+import ToolkitProvider, { CSVExport } from "react-bootstrap-table2-toolkit";
+import "./App.css";
+import { newExpression } from "@babel/types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const { ExportCSVButton } = CSVExport;
+const pricesData = [
+  { id: "1", fruit: "banana", price: "10" },
+  { id: "2", fruit: "apple", price: "20" },
+  { id: "3", fruit: "orange", price: "15" }
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [...pricesData]
+    };
+    this.prices = this.prices.bind(this);
+  }
+
+  prices = action => {
+    if (!action) {
+      return this.state.data;
+    } else {
+      switch (action.actionType) {
+        case "addRow":
+          let newRow = {};
+          newRow.id = this.state.data.length + 1;
+          newRow.fruit = " ";
+          newRow.price = " ";
+          this.setState({ data: [...this.state.data, newRow] });
+          console.log(
+            "addRow action=>the state is: " + JSON.stringify(this.state.data),
+            "the action is: " + JSON.stringify(action)
+          );
+          return this.state.data;
+        case "deleteRow":
+          let new_state = this.state.data.filter(row => row.id !== action.row);
+          console.log(
+            " deleteRow action the state is: " + JSON.stringify(new_state),
+            "the action is: " + JSON.stringify(action)
+          );
+          this.setState({ data: [...new_state] });
+          return this.state.data;
+        default:
+          console.log(
+            " return data action the state is: " +
+              JSON.stringify(this.state.data),
+            "the action is: " + JSON.stringify(action)
+          );
+          return this.state.data;
+      }
+    }
+  };
+  render() {
+    console.log(
+      "from main app componenet the state is: " +
+        JSON.stringify(this.state.data)
+    );
+    return (
+      <div className="App">
+        <RenderExpenseTable data={this.state.data} prices={this.prices} />
+      </div>
+    );
+  }
+}
+
+class RenderExpenseTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [...this.props.data] };
+    //this.props.prices = this.props.prices.bind(this);
+  }
+
+  componentDidMount(prevProps) {
+    if (!this.state.data.length) {
+      console.log("data is empty");
+      this.setState({ data: [...this.props.prices({ action: "data" })] });
+    } else console.log("data is not empty");
+  }
+  render() {
+    console.log(" data is: " + JSON.stringify(this.state.data));
+    let tableData = this.state.data;
+    const columns = [
+      {
+        dataField: "id",
+        text: "Id"
+      },
+      {
+        dataField: "fruit",
+        text: "Fruit Name"
+      },
+      {
+        dataField: "price",
+        text: "Fruit Price"
+      },
+      {
+        dataField: "databasePkey",
+        text: "",
+        editable: false,
+        formatter: (cell, row) => {
+          if (row)
+            return (
+              <button
+                className="btn btn-danger btn-xs border-secondary rounded"
+                onClick={() => {
+                  this.setState(this.state.data, () => {
+                    this.props.prices({
+                      actionType: "deleteRow",
+                      row: row.id
+                    });
+                  });
+                }}
+              >
+                Delete Row
+              </button>
+            );
+          return null;
+        }
+      }
+    ];
+
+    return (
+      <div xs={12} className="col form">
+        <ToolkitProvider
+          keyField="id"
+          data={tableData}
+          columns={columns}
+          exportCSV
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {props => (
+            <div>
+              <div className="d-flex justify-content-around p-2">
+                <ExportCSVButton
+                  className="text-light btn bg-success border-secondary rounded"
+                  {...props.csvProps}
+                >
+                  <span>Export CSV</span>
+                </ExportCSVButton>
+
+                <button
+                  className="btn bg-success text-light rounded"
+                  onClick={() =>
+                    this.setState(tableData, () => {
+                      this.props.prices({ actionType: "addRow" });
+                    })
+                  }
+                >
+                  Add Row
+                </button>
+              </div>
+              <BootstrapTable
+                {...props.baseProps}
+                keyField="id"
+                data={tableData}
+                columns={columns}
+                cellEdit={cellEditFactory({
+                  mode: "click",
+                  onStartEdit: (row, column, rowIndex, columnIndex) => {},
+                  beforeSaveCell: (oldValue, newValue, row, column) => {
+                    if (isNaN(Number(newValue))) {
+                      alert(
+                        "You entered " +
+                          newValue +
+                          " Please Enter numbers Only!!"
+                      );
+                    }
+                  },
+                  afterSaveCell: (oldValue, newValue, row, column) => {}
+                })}
+              />
+            </div>
+          )}
+        </ToolkitProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
